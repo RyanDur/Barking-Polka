803808ca2440280409a49cde011f070197b3bd33
Ryan Durling <ryan.p.durling@gmail.com>
2025-05-13 18:39:03 -0500

    add message event schema

--- frontend/src/components/Chat/MessageEvent.ts
+++ frontend/src/components/Chat/MessageEvent.ts
@@ -1,5 +1,15 @@
+import schema, {type Decoder} from 'schemawax';
+
 export type MessageEvent = {
   voice: string,
   message: string,
   id: string,
 }
+
+export const messageEventSchema: Decoder<MessageEvent> = schema.object({
+  required: {
+    voice: schema.string,
+    message: schema.string,
+    id: schema.string,
+  }
+})
