6c7616c90f0bac8a3ed4b50a9a2b3433f905a223
Ryan Durling <ryan.p.durling@gmail.com>
2025-05-18 22:16:59 -0500

    region instead of endpoint

--- backend_tests/pkgs/test_speach_to_text.py
+++ backend_tests/pkgs/test_speach_to_text.py
@@ -8,12 +8,12 @@ from backend.pkgs.FakeSpeechStreamClient import FakeSpeechStreamClient
 from backend.pkgs.speach_to_text import SpeechToText
 
 speech_key =  os.environ.get('SPEECH_KEY')
-speech_endpoint = f"https://{os.environ.get('SPEECH_REGION')}.api.cognitive.microsoft.com"
+speech_region = os.environ.get('SPEECH_REGION')
 
 class TestSpeechToText(unittest.TestCase):
     def test_speech_to_text(self):
         text = ["hello", "how are you", "I am fine", "thank you", "goodbye"]
-        speech_config = speech_sdk.SpeechConfig(subscription=speech_key, endpoint=speech_endpoint)
+        speech_config = speech_sdk.SpeechConfig(subscription=speech_key, region=speech_region)
         speach_to_text = SpeechToText(
             speech_client=FakeSpeechStreamClient(speech_config, text),
             text_client=AzureTextClient(speech_config)
